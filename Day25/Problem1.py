591c5b584b7aa667cd0520a83e11b8f19dd3f69a
James Cameron <33580646+Equinaut@users.noreply.github.com>
2022-01-27 23:49:52 +0000

    Completed Day 25

---
+++ Day25/Problem1.py
@@ -0,0 +1,59 @@
+grid = []
+#1 = >
+#2 = v
+
+with open("input.txt", "r") as file:
+    for line in file.readlines():
+        grid.append([])
+        for item in line[:-1]:
+            if item==">": grid[-1].append(1)
+            elif item == "v": grid[-1].append(2)
+            else: grid[-1].append(0)
+
+def moveEast():
+    canMove = []
+    for y, line in enumerate(grid):
+        for x, item in enumerate(line):
+            if item!= 1: continue
+            if grid[y][(x+1)%len(line)]==0: canMove.append((x,y))
+
+    for x, y in canMove:
+        grid[y][x] = 0
+        grid[y][(x+1)%len(grid[0])] = 1
+    return len(canMove) > 0
+def moveSouth():
+    canMove = []
+    for y, line in enumerate(grid):
+        for x, item in enumerate(line):
+            if item!=2: continue
+            if grid[(y+1)%len(grid)][x]==0: canMove.append((x,y))
+
+    for x, y in canMove:
+        grid[y][x] = 0
+        grid[(y+1)%len(grid)][x] = 2
+    return len(canMove) >0
+
+for line in grid:
+        for item in line:
+            if item==0: print(".", end="")
+            elif item==1: print(">", end="")
+            elif item==2: print("v", end="")
+        print()
+i=0
+
+couldMove = True
+while couldMove:
+    if i%100==0: print(i)
+    couldMoveEast = moveEast()
+    couldMoveSouth = moveSouth()
+    i+=1
+    if not (couldMoveEast or couldMoveSouth): couldMove=False
+    # print("\n\n")
+    # for line in grid:
+    #     for item in line:
+    #         if item==0: print(".", end="")
+    #         elif item==1: print(">", end="")
+    #         elif item==2: print("v", end="")
+    #     print()
+
+print(f"The answer is {i}")
