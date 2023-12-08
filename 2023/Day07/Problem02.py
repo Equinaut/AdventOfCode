515eb1cf90586e3ccb40f9ae6dfe68dc3c7ee278
James Cameron <33580646+Equinaut@users.noreply.github.com>
2023-12-08 04:54:45 +0000

    Complete Day 7

---
+++ 2023/Day07/Problem02.py
@@ -0,0 +1,46 @@
+def main():
+    ans = 0
+    with open("input.txt") as file:
+        ranks = [[] for _ in range(8)]
+        for line in file:
+            hand, score = line.split(" ")
+            score = int(score)
+            
+            _, rank, hand = getMaxRank(hand, hand)
+            ranks[rank].append((hand, score))
+    j = 1
+    for r in ranks:
+        for c in sorted(r, key = lambda r1: handValue(r1[0])):
+            ans += c[1] * j
+            j += 1
+    return ans
+
+def getMaxRank(hand, originalHand):
+    cardVals = "23456789TQKA"
+    if "J" in hand:
+        newHand = list(hand)
+        answers = []
+        for newCard in cardVals:
+            newHand[hand.index("J")] = newCard
+            newRank = getMaxRank("".join(newHand), originalHand)
+            answers.append((newRank[0], newRank[1], originalHand))
+        return max(answers, key = lambda k: k[1])
+    else:
+        counts = {c: hand.count(c) for c in hand}
+        rank = 0
+        if 5 in counts.values(): rank = 7 # five of a kind
+        elif 4 in counts.values(): rank = 6 # four Of a kind
+        elif 3 in counts.values() and 2 in counts.values(): rank = 5 # full house
+        elif 3 in counts.values(): rank = 4 # three Of a kind
+        elif list(counts.values()).count(2) == 2: rank = 3 # two pair
+        elif list(counts.values()).count(2) == 1: rank = 2 # one pair
+        else: rank = 1 # high card
+        return hand, rank, originalHand
+
+def handValue(hand):
+    values = {"T": 10, "J": 0, "Q": 12, "K": 13, "A": 14}
+    value = sum((15 ** i) * (values[hand[4 - i]] if hand[4 - i] in values else int(hand[4 - i])) for i in range(5))
+    return value
+
+if __name__ == "__main__":
+    print(f"The answer is {main()}")
